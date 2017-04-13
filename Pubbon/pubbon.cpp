f503ab5ea57ea718a638f5bab595161ba05c840a
Yuheng Zou <zouyuheng1998@gmail.com>
2017-04-14 00:07:53 +0800

    formatting code

--- Pubbon/pubbon.cpp
+++ Pubbon/pubbon.cpp
@@ -1,6 +1,7 @@
 #include <Python.h>
 #include <frameobject.h>
 #include <cassert>
+
 #include "llvm_env.hpp"
 #include "translate.hpp"
 #include "pubbon.hpp"
@@ -14,11 +15,13 @@ PyObject *Jit_EvalHelper(std::string state, PyFrameObject *frame) {
 PubbonJittedCode *jittedcode_new_direct() {
 	PubbonJittedCode *new_ob = (PubbonJittedCode *)PubbonJittedCode_Type.tp_alloc(&PubbonJittedCode_Type, 0);
 	assert(new_ob != nullptr);
+
 	new_ob->j_run_count = 0;
 	new_ob->j_failed = false;
 	new_ob->j_evalfunc = nullptr;
 	new_ob->j_evalstate = nullptr;
 	new_ob->j_specialization_threshold = 500;
+
 	return new_ob;
 }
 
@@ -27,6 +30,7 @@ static PyObject *jittedcode_new(PyTypeObject *type, PyObject *args, PyObject *kw
 		PyErr_SetString(PyExc_TypeError, "JittedCode takes no arguments");
 		return nullptr;
 	}
+
 	return (PyObject *)jittedcode_new_direct();
 }
 
@@ -42,6 +46,7 @@ static void jittedcode_dealloc(PyObject *co) {
 bool jit_compile(PyCodeObject *code) {
 	if (strcmp(PyUnicode_AsUTF8(code->co_name), "<module>") == 0) return false;
 	PyObject *extra = nullptr;
+    //use CPython API, otherwise it produce segfault
 	_PyCode_GetExtra((PyObject *)code, coIdx, (void **)&extra);
 	PubbonJittedCode *jittedCode = (PubbonJittedCode *)extra;
 
@@ -54,19 +59,24 @@ bool jit_compile(PyCodeObject *code) {
 static PY_UINT64_T HOT_CODE = 1000;
 
 PyObject *eval_frame(PyFrameObject *frame, int throwflag) {
+    //frame information
 	printf("** myjit is evaluating frame=%p lasti=%d lineno=%d throwflag=%d\n", frame, frame->f_lasti, frame->f_lineno, throwflag);
+
 	PyObject *extra = nullptr;
 	_PyCode_GetExtra((PyObject *)frame->f_code, coIdx, (void **)&extra);
+
 	if (extra == nullptr) {
 		PubbonJittedCode *jitted = jittedcode_new_direct();
 		assert(jitted != nullptr);
 		_PyCode_SetExtra((PyObject *)frame->f_code, coIdx, (PyObject *)jitted);
+
 		jitted->j_run_count++;
 		printf("jitted run_count: %llu\n", jitted->j_run_count);
 	}
 	else if (!throwflag) {
 		PubbonJittedCode *jitted = (PubbonJittedCode *)extra;
 		printf("jitted run_count: %llu\n", jitted->j_run_count);
+
 		if (Py_TYPE(jitted) == &PubbonJittedCode_Type && !jitted->j_failed) {
 			if (jitted->j_evalfunc != nullptr) return jitted->j_evalfunc(jitted->j_evalstate, frame);
 			else if (jitted->j_run_count++ > HOT_CODE) {
@@ -83,6 +93,7 @@ PyObject *install_jit(PyObject *self)
     llvm::InitializeNativeTarget();
     llvm::InitializeNativeTargetAsmPrinter();
     llvm::InitializeNativeTargetAsmParser();
+
 	PyThreadState *tstate = PyThreadState_GET();
 	tstate->interp->eval_frame = eval_frame;
 	Py_RETURN_NONE;
@@ -103,15 +114,51 @@ static PyModuleDef pubbonModule = {
 };
 
 PyTypeObject PubbonJittedCode_Type = {
-	PyVarObject_HEAD_INIT(&PyType_Type, 0)
-	"PubbonJittedCode",
-	sizeof(PubbonJittedCode)
+    PyVarObject_HEAD_INIT(&PyType_Type, 0)
+    "pubbonjittedcode",                 /* tp_name */
+    sizeof(PubbonJittedCode),           /* tp_basicsize */
+    0,                                  /* tp_itemsize */
+    jittedcode_dealloc,                 /* tp_dealloc */
+    0,                                  /* tp_print */
+    0,                                  /* tp_getattr */
+    0,                                  /* tp_setattr */
+    0,                                  /* tp_reserved */
+    0,                                  /* tp_repr */
+    0,                                  /* tp_as_number */
+    0,                                  /* tp_as_sequence */
+    0,                                  /* tp_as_mapping */
+    0,                                  /* tp_hash */
+    0,                                  /* tp_call */
+    0,                                  /* tp_str */
+    0,                                  /* tp_getattro */
+    0,                                  /* tp_setattro */
+    0,                                  /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT,                 /* tp_flags */
+    0,                                  /* tp_doc */
+    0,                                  /* tp_traverse */
+    0,                                  /* tp_clear */
+    0,                                  /* tp_richcompare */
+    0,                                  /* tp_weaklistoffset */
+    0,                                  /* tp_iter */
+    0,                                  /* tp_iternext */
+    0,                                  /* tp_methods */
+    0,                                  /* tp_members */
+    0,                                  /* tp_getset */
+    0,                                  /* tp_base */
+    0,                                  /* tp_dict */
+    0,                                  /* tp_descr_get */
+    0,                                  /* tp_descr_set */
+    0,                                  /* tp_dictoffset */
+    0,                                  /* tp_init */
+    0,                                  /* tp_alloc */
+    jittedcode_new,                     /* tp_new */
 };
 
 PyMODINIT_FUNC PyInit_pubbon(void) {
 	PubbonJittedCode_Type.tp_dealloc = jittedcode_dealloc;
 	PubbonJittedCode_Type.tp_flags = Py_TPFLAGS_DEFAULT;
 	PubbonJittedCode_Type.tp_new = jittedcode_new;
+
 	PyObject *m;
 	if (PyType_Ready(&PubbonJittedCode_Type) < 0) return NULL;
 	m = PyModule_Create(&pubbonModule);
--- Pubbon/pubbon.hpp
+++ Pubbon/pubbon.hpp
@@ -8,9 +8,13 @@ typedef PyObject * (*Py_EvalFunc)(std::string, struct _frame *);
 struct PubbonJittedCode
 {
     PyObject_HEAD
+    //how many times this code already run
     PY_UINT64_T j_run_count;
+    //if failed, interpret this code and do not try to compile again
     bool j_failed;
+    //JIT eval function
     Py_EvalFunc j_evalfunc;
+    //maybe name in LLVM module
     char* j_evalstate;
     PY_UINT64_T j_specialization_threshold;
 };
