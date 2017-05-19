816493e8acbbe16827ad3a4e8f1e1e37e77cb5e8
Univstar <nixy@qq.com>
2017-05-20 00:32:54 +0800

    Implement a simple control flow

--- Pubbon/translate.cpp
+++ Pubbon/translate.cpp
@@ -22,6 +22,8 @@ StructType *PyObjectTy, *PyCodeObjectTy, *PyFrameObjectTy;
 PointerType *PyObjectPtrTy, *PyCodeObjectPtrTy, *PyFrameObjectPtrTy;
 FunctionType *funcType;
 
+Value *TrueCnst, *FalseCnst;
+
 Function *PyIncref;
 Function *PyDecref;
 Function *PyXDecref;
@@ -35,10 +37,14 @@ Function *BinaryXor;
 Function *BinaryOr;
 Function *InplaceAdd;
 Function *InplaceSubtract;
+Function *SequenceContains;
+Function *SequenceNotContains;
+Function *RichCompare;
+Function *AsCond;
 
 void InitializeModule() {
     SMDiagnostic err;
-    std::string source_file("/Users/zouyuheng/workspace/Pubbon/Pubbon/function.ll");
+    std::string source_file("F:\\Programming\\Python Compiler\\Pubbon\\Pubbon\\function.ll");
     TheModule = parseIRFile(source_file, err, TheContext);
     M = TheModule.get();
     PyObjectTy = TheModule->getTypeByName("struct._object");
@@ -57,6 +63,9 @@ void InitializeModule() {
     fpm->add(createGVNPass());
     fpm->add(createCFGSimplificationPass());
     fpm->doInitialization();
+
+    TrueCnst = ConstantExpr::getIntToPtr(ConstantInt::get(Type::getInt64Ty(TheContext), (int64_t)Py_True), PyObjectPtrTy);;
+    FalseCnst = ConstantExpr::getIntToPtr(ConstantInt::get(Type::getInt64Ty(TheContext), (int64_t)Py_False), PyObjectPtrTy);;
 
     PyIncref = M->getFunction("PyIncref");
     fpm->run(*PyIncref);
@@ -84,6 +93,14 @@ void InitializeModule() {
     fpm->run(*InplaceAdd);
     InplaceSubtract = M->getFunction("InplaceSubtract");
     fpm->run(*InplaceSubtract);
+    SequenceContains = M->getFunction("SequenceContains");
+    fpm->run(*SequenceContains);
+    SequenceNotContains = M->getFunction("SequenceNotContains");
+    fpm->run(*SequenceNotContains);
+    RichCompare = M->getFunction("RichCompare");
+    fpm->run(*RichCompare);
+    AsCond = M->getFunction("AsCond");
+    fpm->run(*AsCond);
 }
 
 bool Translate(PyFrameObject *frame) {
@@ -98,16 +115,21 @@ bool Translate(PyFrameObject *frame) {
     int stackDepth = 0;
 	auto byteCode = (_Py_CODEUNIT *)PyBytes_AsString(code->co_code);
     auto size = PyBytes_Size(code->co_code) / sizeof(_Py_CODEUNIT);
+    BasicBlock **label = new BasicBlock *[size];
+    for (int i = 0; i < size; ++i) label[i] = BasicBlock::Create(TheContext);
 
     char *str = PyUnicode_AsUTF8(code->co_name);
     Function *newFunc = Function::Create(funcType, Function::ExternalLinkage, str, M);
-    BasicBlock *BB = BasicBlock::Create(TheContext, "entry", newFunc);
-    Builder.SetInsertPoint(BB);
+    // BasicBlock *BB = BasicBlock::Create(TheContext, "entry", newFunc);
+    // Builder.SetInsertPoint(BB);
 
     for (int i = 0; i < size; ++i) {
         auto opcode = _Py_OPCODE(byteCode[i]);
         auto oparg = _Py_OPARG(byteCode[i]);
         // printf("-- %u %u\n", opcode, oparg);
+        bool continuity = true;
+        label[i]->insertInto(newFunc);
+        Builder.SetInsertPoint(label[i]);
         switch (opcode) {
         case LOAD_CONST: {
             ConstantInt *addressInt = ConstantInt::get(Type::getInt64Ty(TheContext), (int64_t)PyTuple_GetItem(consts, oparg));
@@ -133,6 +155,55 @@ bool Translate(PyFrameObject *frame) {
             Value *val = stack[--stackDepth];
             Builder.CreateStore(val, ptr);
             Builder.CreateCall(PyXDecref, std::vector<Value *>{last});
+            break;
+        }
+        case JUMP_FORWARD: {
+            Builder.CreateBr(label[i + oparg / sizeof(_Py_CODEUNIT) + 1]);
+            continuity = false;
+            break;
+        }
+        case POP_JUMP_IF_FALSE: {
+            Value *val = stack[--stackDepth];
+            Value *cond = Builder.CreateCall(AsCond, std::vector<Value *>{val});
+            cond = Builder.CreateTrunc(cond, Type::getInt1Ty(TheContext));
+            Builder.CreateCall(PyDecref, std::vector<Value *>{val});
+            Builder.CreateCondBr(cond, label[i + 1], label[oparg / sizeof(_Py_CODEUNIT)]);
+            continuity = false;
+            break;
+        }
+        case COMPARE_OP: {
+            if (oparg == PyCmp_EXC_MATCH) {
+                flag = false;
+                break;
+            }
+            Value *right = stack[--stackDepth];
+            Value *left = stack[--stackDepth];
+            Value *cond, *res, *op;
+            switch (oparg) {
+            case PyCmp_IS:
+                cond = Builder.CreateICmpEQ(left, right);
+                res = Builder.CreateSelect(cond, TrueCnst, FalseCnst);
+                Builder.CreateCall(PyIncref, std::vector<Value *>{res});
+                break;
+            case PyCmp_IS_NOT:
+                cond = Builder.CreateICmpNE(left, right);
+                res = Builder.CreateSelect(cond, TrueCnst, FalseCnst);
+                Builder.CreateCall(PyIncref, std::vector<Value *>{res});
+                break;
+            case PyCmp_IN:
+                res = Builder.CreateCall(SequenceContains, std::vector<Value *>{right, left});
+                break;
+            case PyCmp_NOT_IN:
+                res = Builder.CreateCall(SequenceNotContains, std::vector<Value *>{right, left});
+                break;
+            default:
+                op = ConstantInt::get(Type::getInt32Ty(TheContext), (int32_t)oparg);
+                res = Builder.CreateCall(RichCompare, std::vector<Value *>{left, right, op});
+                break;
+            }
+            Builder.CreateCall(PyDecref, std::vector<Value *>{left});
+            Builder.CreateCall(PyDecref, std::vector<Value *>{right});
+            stack[stackDepth++] = res;
             break;
         }
         case BINARY_ADD: {
@@ -205,11 +276,13 @@ bool Translate(PyFrameObject *frame) {
         }
         }
         if (!flag) break;
+        if (i != size - 1 && continuity) Builder.CreateBr(label[i + 1]);
     }
     delete[] stack;
     if (!flag) newFunc->eraseFromParent();
     else
     {
+        newFunc->dump();
         fpm->run(*newFunc);
         newFunc->dump();
     }
